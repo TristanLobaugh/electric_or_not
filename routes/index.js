ae13dd45d1c2699fcf7f17ece28adc78ad4f04c7
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-22 16:38:13 -0400

    end of day
    added cars images, created random generator to select and show image on
    page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("mongodb");
+var mongoClient = mongo.MongoClient;
+var mongoUrl = "mongodb://localhost:27017/electricOrNot";
+var db;
+
+mongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// var cars = db.collection("cars").insert({name:"Buick"});
+	db.collection("cars").find({}).toArray(function(error, carResult){
+		// console.log(carResult);
+	// 	for(var i=0; i < carResult.length; i++){
+	// 		console.log(carResult[i].imageSrc);
+	// 	}
+	var getRandomNum = Math.floor(Math.random() * carResult.length);
+	res.render('index', { carImage: carResult[getRandomNum].imageSrc });
+	});
+
 });
 
 module.exports = router;
